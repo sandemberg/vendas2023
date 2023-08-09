f8f50094e22efed7aa8d61c68c8ed624499bbc1f
sandemberg <sandembergcabral@gmail.com>
2023-08-09 09:58:10 -0300

    feat: create Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import { SafeAreaView } from "react-native"
+import { Button, SafeAreaView } from "react-native"
+import { useState } from "react";
 
 import Login from "./modules/login";
+import Modal from "./shared/components/modal/modal";
+
+
 
 const App = () => {
+
+  const [modalVisible, setModalVisible] = useState(false);
 
   const handleOnClick = () => {
     console.log('PASSOU O CLICK');
@@ -10,9 +16,11 @@ const App = () => {
 
   return (
     <SafeAreaView>
+       <Modal onCloseModal={() => setModalVisible(false)} visible={modalVisible} text='TEXTO TESTE' title='TITULO TESTE'/>
+      <Button title='open' onPress={() => setModalVisible(true)}/>
       <Login />
     </SafeAreaView>
-  );
+  ); 
 };
 
 
---
+++ src/shared/components/modal/Modal.tsx
@@ -0,0 +1,38 @@
+import {Alert, ModalProps as ModalPropsReact, Modal as ModalReact, Text, Pressable, View} from 'react-native';
+
+interface ModalProps extends ModalPropsReact {
+    title?: string,
+    text?: string,
+    onCloseModal?: () => void;  
+}
+
+const Modal = ({title, text, onCloseModal, ...props}: ModalProps) => {
+    
+
+    return (
+        <ModalReact
+            animationType="slide"
+            transparent={true}
+            onRequestClose={() => {
+                Alert.alert('Modal has been closed.');
+            onCloseModal();
+            }}
+            {...props}
+            >
+           
+
+            <View>
+                <View>
+                    <Text>{title}</Text>
+                    <Text>{text}</Text>
+                    <Pressable
+                        onPress={onCloseModal}>
+                        <Text>Hide Modal</Text>
+                    </Pressable>
+                </View>
+            </View>
+        </ModalReact>
+    )
+}
+
+export default Modal
